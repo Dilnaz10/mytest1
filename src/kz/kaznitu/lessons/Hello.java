0befc4c1f122dfc7029a96504fb9b52e40c39d6e
Dilnaz <dilnaz@example.com>
2017-10-06 01:03:40 +0600

    ekinshi commit

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -3,6 +3,8 @@ package kz.kaznitu.lessons;
 
 public class Hello {
     public static void main(String[] args){
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Bul zertkhanalyk zhumystyn maksaty:");
+        System.out.println("GitHub-pen zhumys zhasap uirenu.");
     }
 }
