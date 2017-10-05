398da467a81f9b83c7215fb0152e009a446218a1
Dilnaz <dilnaz@example.com>
2017-10-06 01:38:09 +0600

    experiment1 бутагында Hello.java жане MyMath.java файлдарына тагы озгеристер жасалынды.

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -6,5 +6,13 @@ public class Hello {
         System.out.println("Hello World!");
         System.out.println("Bul zertkhanalyk zhumystyn maksaty:");
         System.out.println("GitHub-pen zhumys zhasap uirenu.");
+
+        int a = MyMath.kvadrat(a: 5);
+        int b = MyMath.kub(a: 5);
+
+        System.out.println("a= " + a);
+        System.out.println("b= " +b);
+
+        System.out.println(MyMath.max(a: 8, b: 9));
     }
 }
